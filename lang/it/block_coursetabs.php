b67c0e8e6370c2a478ac7d8fdf85e896eec5c5ca
Marco Traina <trarco22@outlook.it>
2024-12-09 15:31:08 +0100

    IT language added

---
+++ lang/it/block_coursetabs.php
@@ -0,0 +1,14 @@
+<?php
+$string['pluginname'] = 'Blocco CourseTabs';
+$string['configtitle'] = 'Titolo del blocco';
+$string['defaulttitle'] = 'Navigazione del corso';
+$string['coursetabs:addinstance'] = 'Aggiungi un nuovo blocco CourseTabs';
+$string['coursetabs:myaddinstance'] = 'Aggiungi un nuovo blocco CourseTabs alla mia home di Moodle';
+$string['universe_theme_required'] = 'Questo blocco richiede che il tema Universe sia attivo.';
+$string['tab_file_missing'] = 'Il file delle tab del tema Universe è mancante.';
+$string['error_rendering_tabs'] = 'Si è verificato un errore durante il rendering delle tab.';
+$string['default_courseTabContent'] = 'Tab Predefinita 1';
+$string['default_tab2'] = 'Tab Predefinita 2';
+$string['default_tab3'] = 'Tab Predefinita 3';
+$string['default_tab4'] = 'Tab Predefinita 4';
+$string['default_tab5'] = 'Tab Predefinita 5';
